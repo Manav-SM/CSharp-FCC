214c72a181a3d282fb1f58a7595a1d9e8db4b9a0
Manav Desktop <18483618+Manav-SM@users.noreply.github.com>
2021-06-24 17:50:23 +0530

    calculator basic done

---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            int num = Convert.ToInt32("45");
+            Console.WriteLine(num + 5);
+            */
+            Console.Write("Enter a number1 ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter a number2 ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Num1 = " + num1 + " Num2 = " + num2);
+            Console.WriteLine(num1 + num2);
+
+
+            Console.ReadLine();
+        }
+    }
+}
