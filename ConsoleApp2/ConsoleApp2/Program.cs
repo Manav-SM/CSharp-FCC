deb8fc82154077a511507950e0e795f1cbf1e159
Manav Desktop <18483618+Manav-SM@users.noreply.github.com>
2021-06-24 23:51:20 +0530

    done while loops

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string secretWord = "Giraffe";
+            string guess = "";
+            int guessCount = 0;
+            int guessLimit = 3;
+            bool outOFGuesses = false;
+
+            while (guess != secretWord && !outOFGuesses)
+            {
+                if (guessCount < guessLimit)
+                {
+                    Console.Write("Enter guess: ");
+                    guess = Console.ReadLine();
+                    guessCount++;
+                }
+                else
+                {
+                    outOFGuesses = true;
+                }
+            }
+
+            if (outOFGuesses)
+                {
+                    Console.Write("You Lose");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed right");
+                }
+
+            
+
+
+                Console.ReadLine();
+        }
+    }
+}
