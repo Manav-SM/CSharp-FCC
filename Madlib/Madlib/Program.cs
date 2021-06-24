81bd7309d5456f8f9010404487ebc5ff08fb738f
Manav Desktop <18483618+Manav-SM@users.noreply.github.com>
2021-06-24 17:58:37 +0530

    completed madlibs

---
+++ Madlib/Madlib/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Madlib
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string color, pluralNoun, celebrity;
+
+            Console.Write("Enter a color\n");
+            color = Console.ReadLine();
+            Console.Write("Enter a plural noun\n");
+            pluralNoun = Console.ReadLine();
+            Console.Write("Enter a celebrity name\n");
+            celebrity = Console.ReadLine();
+
+            Console.WriteLine("Roses are " + color);
+            Console.WriteLine(pluralNoun +" are blue");
+            Console.WriteLine("I love " +celebrity);
+
+
+            Console.ReadLine();
+        }
+    }
+}
