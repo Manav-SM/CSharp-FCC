e9be889f1c7e76efe23b59792a9b192b222e48e5
Manav Desktop <18483618+Manav-SM@users.noreply.github.com>
2021-06-24 23:33:10 +0530

    completed for now

--- Calculator/Calculator/Program.cs
+++ Calculator/Calculator/Program.cs
@@ -13,13 +13,54 @@ namespace Calculator
             /*
             int num = Convert.ToInt32("45");
             Console.WriteLine(num + 5);
-            */
+            
             Console.Write("Enter a number1 ");
             double num1 = Convert.ToDouble(Console.ReadLine());
             Console.Write("Enter a number2 ");
             double num2 = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine("Num1 = " + num1 + " Num2 = " + num2);
             Console.WriteLine(num1 + num2);
+            */
+
+            Console.Write("Enter a Number: \n");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter Operator: \n");
+            string op = Console.ReadLine();
+
+            Console.Write("Enter a Number: \n");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            if(op == "+")
+            {
+                Console.WriteLine(num1 + num2);
+            } 
+            else if (op == "-")
+            {
+                Console.WriteLine(num1 - num2);
+            }
+            else if (op == "*")
+            {
+                Console.WriteLine(num1 * num2);
+            }
+            else if (op == "/")
+            {
+                Console.WriteLine(num1 / num2);
+            }
+            else if (op == "%")
+            {
+                Console.WriteLine(num1 % num2);
+            }
+            else
+            {
+                Console.WriteLine("Invalid operator");
+            }
+
+
+
+
+
+
 
 
             Console.ReadLine();
